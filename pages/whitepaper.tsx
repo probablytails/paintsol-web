d75d185651118ed3b70bce6ad2166f854ce628bc
Mitch Downey <mitchldowney@gmail.com>
2024-01-22 21:33:46 -0600

    Add Whitepaper page

--- pages/whitepaper.tsx
+++ pages/whitepaper.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/Whitepaper.module.css";
 
 export default function Whitepaper() {
   return (
@@ -9,6 +9,21 @@ export default function Whitepaper() {
         <title>$PAINT – Whitepaper</title>
         <meta name="description" content="TODO: WHITEPAPER PAGE DESCRIPTION" />
       </Head>
+      <div className="container">
+        <div className="row my-5">
+          <Image
+            unoptimized
+            className={styles['whitepaper-image']}
+            src="/whitepaper.jpg"
+            width={0}
+            height={0}
+            sizes=""
+            alt="$Paint Whitepaper (it's an old picture of MS Paint open with a blank canvas)"
+            style={{ width: '100%', height: 'auto' }}
+            priority
+          />
+        </div>
+      </div>
     </>
   );
 }
