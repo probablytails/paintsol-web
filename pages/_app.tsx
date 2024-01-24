c57fa5c4f4a4abc1c88da00929c62e47acc84d33
Mitch Downey <mitchldowney@gmail.com>
2024-01-24 02:20:26 -0600

    Add Admin login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,37 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import '@/styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import '@/styles/globals.css'
 import "@fortawesome/fontawesome-svg-core/styles.css"
-import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
-import Layout from '@/components/Layout';
+import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
+import Layout from '@/components/Layout'
+import { useRouter } from 'next/router'
+import { getUserInfo } from '@/services/admin'
+
+type UserInfo = {
+  nickname?: string
+  picture?: string
+} | null
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const [userInfo, setUserInfo] = useState<UserInfo>(null)
+
   useEffect(() => {
-    import('bootstrap');
-  }, []);
+    import('bootstrap')
+  }, [])
+
+  useEffect(() => {
+    (async () => {
+      if (localStorage.getItem('loginAttempted')) {
+        const data = await getUserInfo()
+        setUserInfo(data)
+      }
+    })()
+  }, [router.asPath])
+
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
+  )
 }
---
+++ services/admin.ts
@@ -0,0 +1,15 @@
+import { apiRequest } from './apiRequest';
+
+export const getUserInfo = async () => {
+  try {
+    const response = await apiRequest({
+      method: 'get',
+      url: '/admin/userinfo',
+      withCredentials: true
+    })
+    
+    return response?.data || null
+  } catch (error) {
+    return null
+  }
+}
