0d5f05f3ce4d7adfc2dc7ced393f0302b738eef7
Mitch Downey <mitchldowney@gmail.com>
2024-01-24 02:19:49 -0600

    Create apiRequest.ts

---
+++ services/apiRequest.ts
@@ -0,0 +1,8 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export const apiRequest = (config: AxiosRequestConfig<any>) => {
+  return axios.request({
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    ...config
+  })
+}
